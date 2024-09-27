1eef055bc484d8b42d2387a16bce95aa5979bf22
Greyby9 <gregorypc19@gmail.com>
2024-09-27 12:55:30 +0100

    Create login.js

---
+++ login.js
@@ -0,0 +1,16 @@
+<script type="module">
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAwLftNslvPuHAiBy1ew-H6ktLGmG13Obc",
+    authDomain: "limitless-760a4.firebaseapp.com",
+    projectId: "limitless-760a4",
+    storageBucket: "limitless-760a4.appspot.com",
+    messagingSenderId: "271081610858",
+    appId: "1:271081610858:web:77cda074632a33d498b413"
+  };
+
+
+  const app = initializeApp(firebaseConfig);
+</script>
