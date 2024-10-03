7eb64845d2dcf61f7e6075840c9b35cab6841339
Greyby9 <gregorypc19@gmail.com>
2024-10-03 10:58:38 +0100

    login

--- registro.js
+++ registro.js
@@ -47,3 +47,22 @@ document.getElementById('registro').addEventListener('submit', function(event) {
                     document.getElementById('error-message').textContent = errorMessage;
                 });
         });
+
+document.getElementById('login')?.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('emaillog').value;
+    const password = document.getElementById('passwordlog').value;
+
+    auth.signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            const successMessageDiv = document.getElementById('success-message-login');
+            successMessageDiv.textContent = "¡Sesión iniciada correctamente!";
+            successMessageDiv.style.display = "block";
+        })
+        .catch((error) => {
+            const errorMessage = error.message;
+            document.getElementById('error-message-login').textContent = errorMessage;
+        });
+});
