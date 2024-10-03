5bbf035b9c61c0f93955eab3e97b2fafb7654e62
Greyby9 <gregorypc19@gmail.com>
2024-10-03 15:04:00 +0100

    Update registro.js

--- registro.js
+++ registro.js
@@ -1,4 +1,3 @@
-window.onload = function() {
 const firebaseConfig = {
     apiKey: "AIzaSyAwLftNslvPuHAiBy1ew-H6ktLGmG13Obc",
     authDomain: "limitless-760a4.firebaseapp.com",
@@ -49,24 +48,3 @@ document.getElementById('registro').addEventListener('submit', function(event) {
             document.getElementById('error-message').textContent = errorMessage;
         });
 });
-
-// Manejar el inicio de sesión
-document.getElementById('login').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const loginEmail = document.getElementById('emaillog').value;
-    const loginPassword = document.getElementById('passwordlog').value;
-
-    // Iniciar sesión del usuario en Firebase Authentication
-    auth.signInWithEmailAndPassword(loginEmail, loginPassword)
-        .then((userCredential) => {
-            // Usuario autenticado correctamente
-            console.log("Usuario autenticado correctamente.");
-            // Aquí puedes redirigir o mostrar un mensaje de éxito
-        })
-        .catch((error) => {
-            const errorMessage = error.message;
-            document.getElementById('error-message').textContent = errorMessage;
-        });
-});
-};
