37996f7c2185d0041aadb6d5fa977695c96ae136
Greyby9 <gregorypc19@gmail.com>
2024-10-03 15:14:53 +0100

    Update recuperar.js

--- recuperar.js
+++ recuperar.js
@@ -1,1 +1,30 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwLftNslvPuHAiBy1ew-H6ktLGmG13Obc",
+    authDomain: "limitless-760a4.firebaseapp.com",
+    projectId: "limitless-760a4",
+    storageBucket: "limitless-760a4.appspot.com",
+    messagingSenderId: "271081610858",
+    appId: "1:271081610858:web:77cda074632a33d498b413"
+};
+
+// Inicializar Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+        document.getElementById('recuparpass').addEventListener('submit', function(event) {
+            event.preventDefault();
+  
+            const resetEmail = document.getElementById('resetEmail').value;
+  
+            // Enviar correo de recuperación de contraseña
+            auth.sendPasswordResetEmail(resetEmail)
+                .then(() => {
+                    document.getElementById('error-message').textContent = "Correo de recuperación enviado. Revisa tu bandeja de entrada.";
+                })
+                .catch((error) => {
+                    const errorMessage = error.message;
+                    document.getElementById('error-message').textContent = errorMessage;
+                });
+        });
 
